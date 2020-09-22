fccb031f579da0c92d21af24ce69a23d682e256a
Itiel Maimon <itiel.maimon@gmail.com>
2020-09-22 23:18:32 +0300

    Added create and edit functionality

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
 
-export default StreamCreate;
+  render() {
+    return (
+      <div className="ui container">
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
+    );
+  }
+}
+
+export default connect(null, { createStream })(StreamCreate);
